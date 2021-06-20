2b22e8bf2e13557186652625ad9f7a82f860e1f4
MaulidaRiska <maulidariska190@gmail.com>
2021-06-19 18:15:41 -0700

    Pada class Pegawai buat Getter and Setter untuk seluruh atribut

--- src/model/Pegawai.java
+++ src/model/Pegawai.java
@@ -5,16 +5,98 @@ import java.time.*;
 //parentclass/superclass
 public class Pegawai {
     // atribut
-    String idPegawai;
-    String nama;
-    LocalDate tglLahir;
-    String Alamat;
-    String NoTelp;
-    String Status;
-    int gol;
-    int LamaKerja;
-    String User;
-    int Pass;
+    private String idPegawai;
+    private String nama;
+    private LocalDate tglLahir;
+    private String Alamat;
+    private String NoTelp;
+    private String Status;
+    private int gol;
+    private int LamaKerja;
+    private String User;
+    private int Pass;
+
+    //getter dan setter
+    //method (6)
+    public String getIdPegawai() {
+        return idPegawai;
+    }
+    //method (7)
+    public void setIdPegawai(String idPegawai) {
+        this.idPegawai = idPegawai;
+    }
+    //method (8)
+    public String getNama() {
+        return nama;
+    }
+    //method (9)
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    //method (10)
+    public LocalDate getTglLahir() {
+        return tglLahir;
+    }
+    //method (11)
+    public void setTglLahir(LocalDate tglLahir) {
+        this.tglLahir = tglLahir;
+    }
+    //method (12)
+    public String getAlamat() {
+        return Alamat;
+    }
+    //method (13)
+    public void setAlamat(String alamat) {
+        Alamat = alamat;
+    }
+    //method (14)
+    public String getNoTelp() {
+        return NoTelp;
+    }
+    //method (15)
+    public void setNoTelp(String noTelp) {
+        NoTelp = noTelp;
+    }
+    //method (16)
+    public String getStatus() {
+        return Status;
+    }
+    //method (17)
+    public void setStatus(String status) {
+        Status = status;
+    }
+    //method (18)
+    public int getGol() {
+        return gol;
+    }
+    //method (19)
+    public void setGol(int gol) {
+        this.gol = gol;
+    }
+    //method (20)
+    public int getLamaKerja() {
+        return LamaKerja;
+    }
+    //method (21)
+    public void setLamaKerja(int lamaKerja) {
+        LamaKerja = lamaKerja;
+    }
+    //method (22)
+    public String getUser() {
+        return User;
+    }
+    //method (23)
+    public void setUser(String user) {
+        User = user;
+    }
+    //method (24)
+    public int getPass() {
+        return Pass;
+    }
+    //method (25)
+    public void setPass(int pass) {
+        Pass = pass;
+    }
 
     // constructor (1)
     public Pegawai(String idPegawai, String nama, LocalDate tglLahir, String alamat, String noTelp, String status, int gol, int lamaKerja) {
@@ -47,6 +129,7 @@ public class Pegawai {
         System.out.println("Password: "+this.Pass);
         System.out.println("===============================");
     }
+
 
     // method menghitung usia pegawai (1)
     public int hitungUsia(){
