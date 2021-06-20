64e6d1b6fb719d95eb6c8d5a1075ce448b6e81c3
MaulidaRiska <maulidariska190@gmail.com>
2021-06-19 18:08:53 -0700

    Pada class main buat 2 object untuk tiap constructor class Pegawai

--- src/model/main.java
+++ src/model/main.java
@@ -6,6 +6,10 @@ public class main {
 
     public static void main(String[] args) {
 
+        // object constructor 2
+        Pegawai p4 = new Pegawai("Naila");
+        // object constructor 3
+        Pegawai p5 = new Pegawai(67819);
         // object data pegawai (constructor 1)
         Pegawai p3 = new Pegawai();
          p3.idPegawai="PB01";
@@ -17,6 +21,22 @@ public class main {
          p3.gol=1;
          p3.LamaKerja=4;
          p3.printPegawai();
+
+        // object constructor 2
+        Pegawai p1 = new Pegawai("Naila");
+        // object constructor 3
+        Pegawai p2 = new Pegawai(67819);
+        // object data pegawai (constructor 1)
+        Pegawai p6 = new Pegawai();
+        p6.idPegawai="PB01";
+        p6.nama="Nada Salsabila";
+        p6.tglLahir=LocalDate.of(1992, 8, 7);
+        p6.Alamat="Jalan Sidoarjo No. 45";
+        p6.NoTelp="081250827979";
+        p6.Status="Menikah";
+        p6.gol=1;
+        p6.LamaKerja=4;
+        p6.printPegawai();
 
         // object data pegawai dosen (constructor 1)
         Dosen d1 = new Dosen();
